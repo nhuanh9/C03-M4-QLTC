882c1b752e5cbc3c0079caf5220f1e40b0c7061f
Nghia Bui <nghia@MacBook-Air.local>
2023-08-04 14:37:09 +0700

    xong login

--- src/router/userRouter.ts
+++ src/router/userRouter.ts
@@ -6,6 +6,7 @@ const UserRouter = Router();
 // TradeRouter.use(auth)
 UserRouter.get('/users', userController.getMoney)
 UserRouter.post('/login', userController.login);
+UserRouter.post('/register', userController.register);
 
 
 export default UserRouter;
