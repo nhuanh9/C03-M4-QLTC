a88e6f23dc0fe0eee33ca2c9405c55841e60d0ca
Tru Nguyen <nguyentruhn@gmail.com>
2023-08-07 23:20:04 +0700

    sua API tim kiem

--- src/controller/tradeController.ts
+++ src/controller/tradeController.ts
@@ -1,7 +1,6 @@
 import {Request, Response} from "express";
 import TradeService from "../service/tradeService";
 
-
 class TradeController {
     private tradeService;
 
@@ -9,34 +8,35 @@ class TradeController {
         this.tradeService = TradeService;
     }
     findAll = async (req: Request, res: Response) => {
-        let find='';
-        if(req.query.name){
-            find = await this.tradeService.nameTrade(req.query.name)
-            console.log(1)
-        }
-        else if(req.query.date && req.query.type) {
-            find = await this.tradeService.findDate(req.query.date,req.query.type)
-            console.log(5)
-        }
-        else if(req.query.type) {
-            find = await this.tradeService.findByType(req.query.type)
-            console.log(2)
-        }
-        else if(req.query.minAmount && req.query.maxAmount ) {
-            find = await this.tradeService.findByRange(Number(req.query.minAmount),Number(req.query.maxAmount))
-            console.log(3)
-        }
-        else if(req.query.amount) {
-            find = await this.tradeService.sortAmount(req.query.amount)
-            console.log(4)
-        }
+        res.json(await this.tradeService.getTraders(req));
+        // let find='';
+        // if(req.query.name){
+        //     find = await this.tradeService.nameTrade(req.query.name)
+        //     console.log(1)
+        // }
+        // else if(req.query.date && req.query.type) {
+        //     find = await this.tradeService.findDate(req.query.date,req.query.type)
+        //     console.log(5)
+        // }
+        // else if(req.query.type) {
+        //     find = await this.tradeService.findByType(req.query.type)
+        //     console.log(2)
+        // }
+        // else if(req.query.minAmount && req.query.maxAmount ) {
+        //     find = await this.tradeService.findByRange(Number(req.query.minAmount),Number(req.query.maxAmount))
+        //     console.log(3)
+        // }
+        // else if(req.query.amount) {
+        //     find = await this.tradeService.sortAmount(req.query.amount)
+        //     console.log(4)
+        // }
      
-        else{
-            find = await this.tradeService.getAll();
-            console.log(6)
-        }
-        res.json(find);
-        console.log(find)
+        // else{
+        //     find = await this.tradeService.getAll();
+        //     console.log(6)
+        // }
+        // res.json(find);
+        // console.log(find)
     }
     addTrade = async (req: Request, res: Response) => { 
         let data = await this.tradeService.addTrade(req.body)
--- src/service/tradeService.ts
+++ src/service/tradeService.ts
@@ -2,42 +2,63 @@ import { Trade } from "../entity/trade";
 import {AppDataSource} from "../data-source";
 import { Between } from "typeorm";
 import { Like } from "typeorm";
+import {Request} from "express";
 
 class TradeService {
-    private Repository;
+    private repository;
     constructor() {
-        this.Repository = AppDataSource.getRepository(Trade);
+        this.repository = AppDataSource.getRepository(Trade);
     }
     
+    async getTraders(req: Request) {
+        const { date, name, type } = req.query; // data gui len la date, name, type
+        
+        const query = this.repository.createQueryBuilder('trade'); // tao query bang trade
+        if (date) { // neu co data date gui len
+            query.where('date = :date', { date }); // them date vao condition
+        }
+        if (name) { // neu co name gui len
+            query.andWhere('name like :name', { name: `%${name}%` }); // tim kiem gan dung theo name
+        }
+        console.log(type);
+        
+        if (type) {
+            let tradeType = type === '1' ? 'Revenue' : 'Pay'; // neu type gui len = 1 -> Revenue, nguoc lai type -> Pay
+            query.andWhere('type = :tradeType', { tradeType });
+        }
+        
+        return await query.getMany();
+    }
+
     getAll = async () => {
-        return await this.Repository.find()
+        return await this.repository.find()
     }
     nameTrade = async (name) => {
-        return await this.Repository.find({
+        return await this.repository.find({
             where : {
                 name: Like(`%${name}%`),
             }
         })
     }
     findByType = async (type) => {
-        return await this.Repository.find({
+        return await this.repository.find({
             where : {
                 type :type
             }
         })
     }
     addTrade = async (trade) => {
-        await this.Repository.save(trade)
+        await this.repository.save(trade)
     }
     findByRange = async (minAmount:number, maxAmount:number) => {
-         return await this.Repository.find({
+         return await this.repository.find({
             where: {
                amount: Between (minAmount, maxAmount) ,
             }
           });   
       }
     findDate = async (date,type) => {
-        return await this.Repository.find({
+        return await this.repository.find({
             where : {
                 date : date,
                 type : type
@@ -45,14 +66,14 @@ class TradeService {
         })
     }
     sortAmount = async (amount) => {
-        return await this.Repository.find(
+        return await this.repository.find(
             {
                 order: {amount: amount}
         })
     }
     findMonth = async (month) => {
         try {
-          const tradeData = await this.Repository.createQueryBuilder('trade')
+          const tradeData = await this.repository.createQueryBuilder('trade')
             .where(`MONTH(trade.date) = :month`, { month })
             .getMany();
           return tradeData;
